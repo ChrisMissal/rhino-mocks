8eb44bb1044a1f111b3ab9471b504403b29b19c5
Chris Missal <chris.missal@gmail.com>
2009-12-08 00:18:37 -0600

    Added another example test and some sample classes

--- Rhino.Mocks.GettingStarted/Stubs.cs
+++ Rhino.Mocks.GettingStarted/Stubs.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Rhino.Mocks.GettingStarted
 {
@@ -35,6 +36,27 @@ namespace Rhino.Mocks.GettingStarted
         }
 
         /// <summary>
+        /// Asserting that an instance inside your system under test is called with a parameter
+        /// matching a set of expected criteria. This example shows that our FooService should
+        /// call it's dependency's method with the Name property of the Foo object passed into
+        /// the LookUpFoo method.
+        /// </summary>
+        [Test]
+        public void How_to_Assert_that_a_method_calls_an_expected_method_with_value()
+        {
+            // Arrange
+            var foo = new Foo {Name = "rhino-mocks"};
+            var mockFooRepository = MockRepository.GenerateStub<IFooRepository>();
+            var fooService = new FooService(mockFooRepository);
+
+            // Act
+            fooService.LookUpFoo(foo);
+
+            // Assert
+            mockFooRepository.AssertWasCalled(x => x.GetFooByName(Arg<string>.Matches(y => y.Equals(foo.Name))));
+        }
+
+        /// <summary>
         /// This is just a sample interface, what it is or does isn't really relevant. It could
         /// be IUser of IOrder
         /// </summary>
@@ -54,6 +76,31 @@ namespace Rhino.Mocks.GettingStarted
             }
 
             public string Name { get; set; }
+        }
+
+        public interface IFooService
+        {
+            IFoo LookUpFoo(IFoo foo);
+        }
+
+        public class FooService : IFooService
+        {
+            private readonly IFooRepository fooRepository;
+
+            public FooService(IFooRepository fooRepository)
+            {
+                this.fooRepository = fooRepository;
+            }
+
+            public virtual IFoo LookUpFoo(IFoo foo)
+            {
+                return fooRepository.GetFooByName(foo.Name);
+            }
+        }
+
+        public interface IFooRepository
+        {
+            IFoo GetFooByName(string name);
         }
     }
 }
